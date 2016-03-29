b7c7ce15f0d3a170fed8ea6fd90204ccc5929971
Isaac <provenchez@gmail.com>
2016-03-29 19:42:53 -0400

    HEROKUUU

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 server = Flask(__name__)
 
@@ -6,5 +7,5 @@ def main():
     return "flask app responding hello world !"
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     server.run(host='0.0.0.0', port=port)
