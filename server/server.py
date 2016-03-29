b139c7cfbf30f409dc91c303d6cc50cb598678d7
Isaac <provenchez@gmail.com>
2016-03-29 19:39:40 -0400

    port shanenegans

--- server/server.py
+++ server/server.py
@@ -6,4 +6,5 @@ def main():
     return "flask app responding hello world !"
 
 if __name__ == "__main__":
-	server.run(host="0.0.0.0")
+	port = int(os.environ.get('PORT', 5000))
+    server.run(host='0.0.0.0', port=port)
